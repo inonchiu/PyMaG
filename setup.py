eca04a18ea22cc977a343ddb8ee8d9175f3f0d0c
inonchiu <inonchiu@usm.lmu.de>
2016-06-15 20:38:01 +0800

    add setup.py
    add setup py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pymag',
+    version='0.0',
+    packages=['*',
+              'utils',
+              ],
+      package_data={
+      },
+    url='',
+    license='',
+    author='I-Non Chiu',
+    author_email='inchiu@asiaa.sinica.edu.tw',
+    description='Python package of Magnification in Galaxy clusters'
+)
